1a9ed0ac632cd6b7e579f739b2eb0f6009d7ab90
David Northey <dnorthey88@gmail.com>
2025-12-10 02:09:35 +1100

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -27,24 +27,29 @@ SELECT
 	COUNT(*)
 FROM silver.crm_cust_info
 GROUP BY cst_id
-HAVING COUNT(*) > 1 OR cst_id IS NULL
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
 -- Check for unwanted spaces
 -- Expectation: No Results
+SELECT 
+	cst_key
+FROM silver.crm_cust_info
+	WHERE cst_key != TRIM(cst_key);
+	
 SELECT cst_firstname
 FROM silver.crm_cust_info
-WHERE cst_firstname != TRIM(cst_firstname)
+WHERE cst_firstname != TRIM(cst_firstname);
 
 SELECT cst_lastname
 FROM silver.crm_cust_info
-WHERE cst_lastname != TRIM(cst_lastname)
+WHERE cst_lastname != TRIM(cst_lastname);
 
 -- Data standardisation & Consistency
 SELECT DISTINCT cst_marital_status
-FROM silver.crm_cust_info
+FROM silver.crm_cust_info;
 
 SELECT DISTINCT cst_gndr
-FROM silver.crm_cust_info
+FROM silver.crm_cust_info;
 
 -- =================================================================================
 -- Checking 'silver.crm_pr_info'
@@ -55,36 +60,49 @@ SELECT prd_id,
 COUNT(*)
 FROM silver.crm_prd_info
 GROUP BY prd_id
-HAVING COUNT(*) > 1 OR prd_id IS NULL
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
 -- Check for unwanted spaces
 -- Expectation: No Results
 SELECT prd_nm
 FROM silver.crm_prd_info
-WHERE prd_nm != TRIM(prd_nm)
+WHERE prd_nm != TRIM(prd_nm);
 
 -- Check for NULLS or Negative Numbers
 -- Expectation: No Results
 SELECT prd_cost
 FROM silver.crm_prd_info
-WHERE prd_cost < 0 OR prd_cost IS NULL
+WHERE prd_cost < 0 OR prd_cost IS NULL;
 
 -- Data Standardisation & Consistency
 SELECT DISTINCT prd_line
-FROM silver.crm_prd_info
+FROM silver.crm_prd_info;
 
 -- Check for Invalid Date Orders
 SELECT *
 FROM silver.crm_prd_info
-WHERE prd_end_dt < prd_start_dt
+WHERE prd_end_dt < prd_start_dt;
 
 -- =================================================================================
 -- Checking 'silver.crm_sales_details'
 -- =================================================================================
--- Check for Invalid Date Orders
+-- Check for Invalid Dates
+-- Expectation: No Invalid Dates
+SELECT
+	NULLIF(sls_due_dt, 0) AS sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_due_dt <= 0
+	OR LEN(sls_due_dt) != 8
+	OR sls_due_dt > 20500101
+	OR sls_due_dt < 19000101;
+
+	
+-- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
+-- Expectation: No Results
 SELECT *
 FROM silver.crm_sales_details
-WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+WHERE sls_order_dt > sls_ship_dt 
+	OR sls_order_dt > sls_due_dt;
 
 -- Check Data Consistency: Between Sales, Quantity and Price
 -- >> Sales = Quality * Price
@@ -98,7 +116,7 @@ FROM silver.crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price
 OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
 OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
-ORDER BY sls_sales, sls_quantity, sls_price
+ORDER BY sls_sales, sls_quantity, sls_price;
 
 -- =================================================================================
 -- Checking 'silver.erp_cust_az12'
@@ -107,12 +125,13 @@ ORDER BY sls_sales, sls_quantity, sls_price
 SELECT DISTINCT
 bdate
 FROM silver.erp_cust_az12
-WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+WHERE bdate < '1924-01-01' 
+	OR bdate > GETDATE();
 
 -- Data Standardisation & Consistency
 SELECT DISTINCT
 gen 
-FROM silver.erp_cust_az12
+FROM silver.erp_cust_az12;
 
 -- =================================================================================
 -- Checking 'silver.erp_loc_a101'
@@ -120,7 +139,7 @@ FROM silver.erp_cust_az12
 -- Data Standardisation & Consistency
 SELECT DISTINCT cntry
 FROM silver.erp_loc_a101
-ORDER BY cntry
+ORDER BY cntry;
 
 -- =================================================================================
 -- Checking 'silver.erp_px_cat_g1v2'
@@ -129,9 +148,11 @@ ORDER BY cntry
 -- Expectation: No Results
 SELECT * 
 FROM silver.erp_px_cat_g1v2
-WHERE cat != TRIM(CAT) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
+WHERE cat != TRIM(CAT) 
+	OR subcat != TRIM(subcat) 
+	OR maintenance != TRIM(maintenance);
 
 -- Data Standardisation & Consistency
 SELECT DISTINCT
 maintenance
-FROM silver.erp_px_cat_g1v2
+FROM silver.erp_px_cat_g1v2;
